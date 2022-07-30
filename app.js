a868200b35aa06e9f4571f25ce80eb6038bb30bd
talhaarif <108628697+MTalhaArif@users.noreply.github.com>
2022-07-30 19:42:00 +0500

    Changed commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require('express');
 const mongoose=require('mongoose');
 
 const Blogs= require('./models/blogs')
-//creating express app
+//creating express app files
 const app=express();
 
 //connect to mongo db
